1c412a4ff61809c3313dcf2fc98e9229565ffc63
duribehz <duribehz@gmail.com>
2026-05-22 14:24:44 -0600

    feat: add XLS controller route

--- src/routes/xls.routes.js
+++ src/routes/xls.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/xls.controller")
 
-router.get("/")
+router.get("/", controller)
 
 module.exports = router;
